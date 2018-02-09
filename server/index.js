675ed2134d8a68031976f31b7a6a5efc8a0bd173
Ghost <ghost@Ghosts-MacBook-Pro.local>
2018-02-08 21:22:24 -0500

    changed message

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: "everyone" })
+    res.send({ hi: "friends" })
 })
 
 const PORT = process.env.PORT || 5000
